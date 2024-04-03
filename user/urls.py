b2d3984cce78bf6007be72c0637dc7fe42e6f26b
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-04-03 18:08:22 +0300

    Add new URL with name token_obtain_pair to urlpatterns in urls.py

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from user.views import CreateUserView
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="register"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
 ]
 
 app_name = "user"
