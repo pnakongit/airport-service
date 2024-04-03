bb375968ba14553aa6a4760ef84b9dfb8f1c60f5
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-04-03 19:38:52 +0300

    Add ManageUserView to views.py in user app

--- user/views.py
+++ user/views.py
@@ -1,7 +1,19 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
+from user.models import User
 from user.serializers import UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self) -> User:
+        return self.request.user
