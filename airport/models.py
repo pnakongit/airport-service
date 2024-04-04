523b3564916390cb428621a3dc179320259e2e82
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-04-04 18:29:54 +0300

    Add City model to models.py in airport app

--- airport/models.py
+++ airport/models.py
@@ -9,3 +9,14 @@ class Country(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class City(models.Model):
+    name = models.CharField(max_length=25)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name="cities")
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
