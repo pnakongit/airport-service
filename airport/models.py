a4c5255fcd8cee4ea541ec178259fca2383e79a0
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-04-04 18:24:55 +0300

    Add Country model to models.py in airport app

--- airport/models.py
+++ airport/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=25)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
