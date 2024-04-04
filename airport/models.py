1a4d84b1d3bf9940b0f4019c910788f38accd3de
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-04-04 18:37:17 +0300

    Add Airport model to models.py in airport app

--- airport/models.py
+++ airport/models.py
@@ -20,3 +20,20 @@ class City(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class Airport(models.Model):
+    name = models.CharField(max_length=25)
+    closest_big_city = models.ForeignKey(
+        City,
+        on_delete=models.SET_NULL,
+        related_name="airports",
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
