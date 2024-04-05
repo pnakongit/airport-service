955cab3e6a5c7a6338249bdd622d888280db0e83
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-04-05 15:40:44 +0300

    Add CitySerializer to serializers.py in airport app

--- airport/serializers.py
+++ airport/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from airport.models import Country
+from airport.models import Country, City
 
 
 class CountrySerializer(serializers.ModelSerializer):
     class Meta:
         model = Country
         fields = ("id", "name")
+
+
+class CitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = City
+        fields = ("id", "name", "country")
