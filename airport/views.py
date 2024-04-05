12ae7b195df6b235fa27383f142289b03558b3e2
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-04-05 15:58:54 +0300

    Add CityViewSet to views.py in airport app

--- airport/views.py
+++ airport/views.py
@@ -1,9 +1,22 @@
-from rest_framework import viewsets
+from typing import Type
 
-from airport.models import Country
-from airport.serializers import CountrySerializer
+from rest_framework import viewsets
+from rest_framework.serializers import Serializer
+
+from airport.models import Country, City
+from airport.serializers import CountrySerializer, CitySerializer, CityListDetailSerializer
 
 
 class CountryViewSet(viewsets.ModelViewSet):
     serializer_class = CountrySerializer
     queryset = Country.objects.all()
+
+
+class CityViewSet(viewsets.ModelViewSet):
+    serializer_class = CitySerializer
+    queryset = City.objects.all()
+
+    def get_serializer_class(self) -> Type[Serializer]:
+        if self.action in ["list", "retrieve"]:
+            return CityListDetailSerializer
+        return super().get_serializer_class()
