b015f5b43157fc8a72b94d065366e86dabd12896
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-04-05 14:27:17 +0300

    Register Airport model in admin.py

--- airport/admin.py
+++ airport/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from airport.models import Country, City
+from airport.models import (
+    Country,
+    City,
+    Airport
+)
 
 admin.site.register(Country)
 admin.site.register(City)
+admin.site.register(Airport)
