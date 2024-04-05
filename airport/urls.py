45ab756b2d9109009bb0278b514a6272cfae4685
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-04-05 16:00:01 +0300

    Register CityViewSet with prefix cities in urls.py in airport app

--- airport/urls.py
+++ airport/urls.py
@@ -1,9 +1,11 @@
 from rest_framework import routers
 
-from airport.views import CountryViewSet
+from airport.views import CountryViewSet, CityViewSet
 
 router = routers.DefaultRouter()
+
 router.register("countries", CountryViewSet, basename="country")
+router.register("cities", CityViewSet, basename="city")
 
 urlpatterns = router.urls
 
