61a0801812047983cccd0e316d4e43c3edb7bb16
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-04-05 16:22:10 +0300

    Optimise sql queries for list action in CityViewSet

--- airport/views.py
+++ airport/views.py
@@ -1,5 +1,6 @@
 from typing import Type
 
+from django.db.models import QuerySet
 from rest_framework import viewsets
 from rest_framework.serializers import Serializer
 
@@ -15,6 +16,12 @@ class CountryViewSet(viewsets.ModelViewSet):
 class CityViewSet(viewsets.ModelViewSet):
     serializer_class = CitySerializer
     queryset = City.objects.all()
+
+    def get_queryset(self) -> QuerySet:
+        city_qs = super().get_queryset()
+        if self.action == "list":
+            return city_qs.select_related("country")
+        return city_qs
 
     def get_serializer_class(self) -> Type[Serializer]:
         if self.action in ["list", "retrieve"]:
