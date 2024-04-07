1b8da3adb07ec2470c99d0b0dba9a89b11c295cd
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-04-07 13:30:54 +0300

    Add AirportViewSet to views.py in airport app

--- airport/views.py
+++ airport/views.py
@@ -4,8 +4,14 @@ from django.db.models import QuerySet
 from rest_framework import viewsets
 from rest_framework.serializers import Serializer
 
-from airport.models import Country, City
-from airport.serializers import CountrySerializer, CitySerializer, CityListDetailSerializer
+from airport.models import Country, City, Airport
+from airport.serializers import (
+    CountrySerializer,
+    CitySerializer,
+    CityListDetailSerializer,
+    AirportSerializer,
+    AirportListDetailSerializer
+)
 
 
 class CountryViewSet(viewsets.ModelViewSet):
@@ -26,4 +32,19 @@ class CityViewSet(viewsets.ModelViewSet):
     def get_serializer_class(self) -> Type[Serializer]:
         if self.action in ["list", "retrieve"]:
             return CityListDetailSerializer
+        return super().get_serializer_class()
+
+
+class AirportViewSet(viewsets.ModelViewSet):
+    serializer_class = AirportSerializer
+    queryset = Airport.objects.all()
+
+    def get_queryset(self) -> QuerySet:
+        airport_qs = super().get_queryset()
+        if self.action == "list":
+            return airport_qs.select_related("closest_big_city")
+
+    def get_serializer_class(self) -> Type[Serializer]:
+        if self.action in ["list", "retrieve"]:
+            return AirportListDetailSerializer
         return super().get_serializer_class()
