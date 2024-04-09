1472144c349bf5796dde8530c2e5eed0b88527c7
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-04-09 19:52:46 +0300

    Add REST_FRAMEWORK with pagination settings in settings.py

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -133,6 +133,11 @@ MEDIA_ROOT = "media/"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 4
+}
+
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=360),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=5),
