6eba9d6e208fe7fad9eaa569138ff6b7060961bd
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-04-09 10:54:23 +0300

    Register TicketNestedViewSet as nested router for OrderViewSet

--- airport/urls.py
+++ airport/urls.py
@@ -1,5 +1,6 @@
+from django.urls import path, include
 from rest_framework import routers
-
+from rest_framework_nested import routers as nested_routers
 from airport.views import (
     CountryViewSet,
     CityViewSet,
@@ -9,7 +10,8 @@ from airport.views import (
     FlightViewSet,
     AirplaneViewSet,
     AirplaneTypeViewSet,
-    OrderViewSet
+    OrderViewSet,
+    TicketNestedViewSet
 )
 
 router = routers.DefaultRouter()
@@ -24,6 +26,12 @@ router.register("airplanes", AirplaneViewSet, basename="airplane")
 router.register("airplane-types", AirplaneTypeViewSet, basename="airplane_type")
 router.register("orders", OrderViewSet, basename="orders")
 
-urlpatterns = router.urls
+orders_router = nested_routers.NestedSimpleRouter(router, "orders", lookup="order")
+orders_router.register("tickets", TicketNestedViewSet, basename="order_ticket")
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path("", include(orders_router.urls))
+]
 
 app_name = "airport"
