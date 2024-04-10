4aaa1b9b6506a64be41111bfb37830815ad152ac
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-04-10 18:40:14 +0300

    Add DEFAULT_AUTHENTICATION_CLASSES with JWTAuthentication to settings.py

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -134,6 +134,9 @@ MEDIA_ROOT = "media/"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication"
+    ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 4
 }
