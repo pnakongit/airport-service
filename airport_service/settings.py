2cf85706830fd3a372486843e0c1b6d14a396c95
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-04-10 20:21:23 +0300

    Change DEFAULT_PERMISSION_CLASSES to IsAdminUser

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -138,7 +138,7 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication"
     ],
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated"
+        "rest_framework.permissions.IsAdminUser"
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 4
