91cd112f1e8bf8dc5ee285074a392c536087e90b
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-04-10 20:22:21 +0300

    Make ManageUserView avaible for authenticated users

--- user/views.py
+++ user/views.py
@@ -1,6 +1,5 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from user.models import User
 from user.serializers import UserSerializer
@@ -13,6 +12,7 @@ class CreateUserView(generics.CreateAPIView):
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_object(self) -> User:
         return self.request.user
