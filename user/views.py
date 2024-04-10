a618fdd0f99d28af199cee3c9d66a2d8489e2ba1
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-04-10 18:48:04 +0300

    Remove reduant authentication_classes & permission_classes from ManageUserView

--- user/views.py
+++ user/views.py
@@ -13,8 +13,6 @@ class CreateUserView(generics.CreateAPIView):
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
-    authentication_classes = (JWTAuthentication,)
-    permission_classes = (IsAuthenticated,)
 
     def get_object(self) -> User:
         return self.request.user
