339e0ae9e4e069edee1de1a9a0f122eedb03d5d3
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-04-12 17:11:05 +0300

    Add AuthenticatedAirportViewTest to test_airport_api.py

--- airport/tests/test_airport_api.py
+++ airport/tests/test_airport_api.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
@@ -11,7 +12,6 @@ AIRPORT_DETAIL_VIEW_NAME = "airport:airport-detail"
 
 class UnAuthorizedAirportAPITest(TestCase):
     def setUp(self) -> None:
-
         airport = sample_airport()
 
         self.detail_url = detail_url(
@@ -39,3 +39,39 @@ class UnAuthorizedAirportAPITest(TestCase):
     def test_airport_delete_auth_required(self) -> None:
         response = self.client.delete(self.detail_url)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class AuthenticatedAirportViewTest(TestCase):
+
+    def setUp(self) -> None:
+        user = get_user_model().objects.create_user(
+            email="user@user.com",
+            password="password123",
+        )
+        self.airport = sample_airport()
+        self.detail_url = detail_url(
+            AIRPORT_DETAIL_VIEW_NAME,
+            self.airport.id
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=user)
+
+    def test_airport_list_permission_denied(self) -> None:
+        response = self.client.get(AIRPORT_URL)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_airport_create_permission_denied(self) -> None:
+        response = self.client.post(AIRPORT_URL)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_airport_detail_permission_denied(self) -> None:
+        response = self.client.get(self.detail_url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_airport_update_permission_denied(self) -> None:
+        response = self.client.put(self.detail_url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_airport_delete_permission_denied(self) -> None:
+        response = self.client.delete(self.detail_url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
