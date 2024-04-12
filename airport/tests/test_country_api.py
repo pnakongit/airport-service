69ea36947bdccc2b16cd708b57fe24070370778d
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-04-12 13:14:53 +0300

    Add AuthenticatedCountryApiTest to test_country_api.py

--- airport/tests/test_country_api.py
+++ airport/tests/test_country_api.py
@@ -43,3 +43,37 @@ class UnAuthenticatedCountryApiTest(TestCase):
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
+class AuthenticatedCountryApiTest(TestCase):
+
+    def setUp(self) -> None:
+        user = get_user_model().objects.create_user(
+            email="user@user.com",
+            password="password123",
+        )
+        self.country = Country.objects.create(
+            name="Test Country"
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=user)
+
+    def test_country_list_permission_denied(self) -> None:
+        response = self.client.get(COUNTRY_URL)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_country_create_permission_denied(self) -> None:
+        response = self.client.post(COUNTRY_URL)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_country_detail_permission_denied(self) -> None:
+        response = self.client.get(detail_url(self.country.id))
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_country_update_permission_denied(self) -> None:
+        response = self.client.put(detail_url(self.country.id))
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_country_delete_permission_denied(self) -> None:
+        response = self.client.delete(detail_url(self.country.id))
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
