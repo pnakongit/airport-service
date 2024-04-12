eff310d657afda5a36762c6a6dc55f1d980b35d9
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-04-12 15:57:02 +0300

    Use get_or_create in sample_city func in helpers.py

--- airport/tests/helpers.py
+++ airport/tests/helpers.py
@@ -2,8 +2,8 @@ from airport.models import City, Country
 
 
 def sample_city(**params) -> City:
-    country = Country.objects.create(
-        name="Test Sample Country Name",
+    country, _ = Country.objects.get_or_create(
+       name="Test Sample Country Name"
     )
     default_city_params = {
         "name": "Test Sample City Name",
