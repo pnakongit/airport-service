4149fd25c359fc05dd5aa083354dd50da3c4f249
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-04-15 17:47:14 +0300

    Add AdminCrewApiTest to test_crew_api.py

--- airport/tests/test_crew_api.py
+++ airport/tests/test_crew_api.py
@@ -5,6 +5,7 @@ from rest_framework import status
 from rest_framework.test import APIClient
 
 from airport.models import Crew
+from airport.serializers import CrewSerializer
 from airport.tests.helpers import detail_url
 
 CREW_URL = reverse("airport:crew-list")
@@ -81,3 +82,107 @@ class AuthenticatedCrewApiTest(TestCase):
     def test_crew_delete_permission_denied(self) -> None:
         response = self.client.delete(self.detail_url)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class AdminCrewApiTest(TestCase):
+    def setUp(self) -> None:
+        admin_user = get_user_model().objects.create_user(
+            email="admin_user@user.com",
+            password="password123",
+        )
+        admin_user.is_staff = True
+        admin_user.save()
+        self.crew = Crew.objects.create(
+            first_name="John",
+            last_name="Doe",
+        )
+        self.detail_url = detail_url(
+            CREW_DETAIL_VIEW_NAME,
+            self.crew.id
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=admin_user)
+
+    def test_crew_list(self) -> None:
+        Crew.objects.create(
+            first_name="Test first name 1",
+            last_name="Test last name 1",
+        )
+        Crew.objects.create(
+            first_name="Test first name 2",
+            last_name="Test last name 2",
+        )
+
+        response = self.client.get(CREW_URL)
+        crew_qs = Crew.objects.all()
+        serializer = CrewSerializer(crew_qs, many=True)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["results"], serializer.data)
+
+    def test_crew_list_is_paginated(self) -> None:
+        page_size = 4
+        crew_count = Crew.objects.count()
+
+        for i in range(page_size - crew_count + 1):
+            Crew.objects.create(
+                first_name=f"Test first name {i}",
+                last_name=f"Test last name {i}"
+            )
+
+        first_page = Crew.objects.all()[:page_size]
+        second_page = Crew.objects.all()[page_size:]
+
+        response = self.client.get(CREW_URL)
+        serializer = CrewSerializer(first_page, many=True)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["results"], serializer.data)
+
+        response = self.client.get(CREW_URL + "?page=" + str(2))
+        serializer = CrewSerializer(second_page, many=True)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["results"], serializer.data)
+
+    def test_crew_detail(self) -> None:
+        response = self.client.get(self.detail_url)
+        serializer = CrewSerializer(self.crew)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_crew_create(self) -> None:
+        payload = {
+            "first_name": "Test first name 1",
+            "last_name": "Test last name 1",
+        }
+
+        response = self.client.post(CREW_URL, payload)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        created_crew = Crew.objects.get(id=response.data["id"])
+
+        for key, value in payload.items():
+            self.assertEqual(getattr(created_crew, key), value)
+
+    def test_crew_update(self) -> None:
+        crew = self.crew
+        payload = {
+            "id": crew.id,
+            "first_name": crew.first_name + "new",
+            "last_name": crew.last_name + "new",
+        }
+
+        response = self.client.put(self.detail_url, payload)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        crew.refresh_from_db()
+
+        for key, value in payload.items():
+            self.assertEqual(getattr(crew, key), value)
+
+    def test_crew_delete(self) -> None:
+        response = self.client.delete(self.detail_url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(
+            Crew.objects.filter(id=self.crew.id).exists()
+        )
