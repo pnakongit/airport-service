8a236355ca5b83fadf611bfce5aef2362007cebf
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-04-15 17:34:15 +0300

    Add AuthenticatedCrewApiTest to test_crew_api.py

--- airport/tests/test_crew_api.py
+++ airport/tests/test_crew_api.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
@@ -42,3 +43,41 @@ class UnAuthenticatedCrewAPITest(TestCase):
     def test_crew_delete_auth_required(self) -> None:
         response = self.client.delete(self.detail_url)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class AuthenticatedCrewApiTest(TestCase):
+    def setUp(self) -> None:
+        user = get_user_model().objects.create_user(
+            email="user@user.com",
+            password="password1234"
+        )
+        self.crew = Crew.objects.create(
+            first_name="John",
+            last_name="Doe",
+        )
+        self.detail_url = detail_url(
+            CREW_DETAIL_VIEW_NAME,
+            self.crew.pk
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=user)
+
+    def test_crew_list_permission_denied(self) -> None:
+        response = self.client.get(CREW_URL)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_crew_detail_permission_denied(self) -> None:
+        response = self.client.get(self.detail_url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_crew_create_permission_denied(self) -> None:
+        response = self.client.post(CREW_URL)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_crew_update_permission_denied(self) -> None:
+        response = self.client.put(self.detail_url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_crew_delete_permission_denied(self) -> None:
+        response = self.client.delete(self.detail_url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
