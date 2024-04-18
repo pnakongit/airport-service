8177690474dd3d0b7df29ffe4f51f8685d10a223
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-04-18 16:07:10 +0300

    Refactor urlpatterns to concatenate router and orders_router urls

--- airport/urls.py
+++ airport/urls.py
@@ -1,4 +1,3 @@
-from django.urls import path, include
 from rest_framework import routers
 from rest_framework_nested import routers as nested_routers
 from airport.views import (
@@ -29,9 +28,6 @@ router.register("orders", OrderViewSet, basename="order")
 orders_router = nested_routers.NestedSimpleRouter(router, "orders", lookup="order")
 orders_router.register("tickets", TicketNestedViewSet, basename="order-ticket")
 
-urlpatterns = [
-    path("", include(router.urls)),
-    path("", include(orders_router.urls))
-]
+urlpatterns = router.urls + orders_router.urls
 
 app_name = "airport"
